f90f573efcd5ac1116278e74349c6803c1879d75
Ted <wodf410@gmail.com>
2022-11-01 00:01:04 +0800

    margin auto

--- src/components/block4Solution.jsx
+++ src/components/block4Solution.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function Block4Solution() {
   return (
-    <div>
-      <h1>Block4Solution</h1>
+    <div className='w-fit m-auto'>
+      <div className='flex'>
+        <h2>WEEK 1</h2>
+        <div>
+          <div>THE F2E</div>
+          <div>活動網站設計</div>
+        </div>
+      </div>
+      <div>
+        <span>視差滾動</span>
+        <span>#板塊設計</span>
+      </div>
+      <button>
+        <span>查看關卡細節</span>
+      </button>
     </div>
   )
 }
