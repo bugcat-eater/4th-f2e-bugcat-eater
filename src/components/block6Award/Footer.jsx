d220a9b26ffef513d415b095b3dcb1444ab8d952
ker ker <kerke.2011@gmail.com>
2022-11-03 00:17:08 +0800

    add Section 6 (Award)

---
+++ src/components/block6Award/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+      <a href="/" className="text-center block text-white bg-black w-fit mx-auto p-5 rounded-full">立即報名</a>
+    </div>
+  )
+}
---
+++ src/components/block6Award/Review.jsx
@@ -0,0 +1,9 @@
+export default function Review() {
+  return (
+    <div className="m-5 p-5 border border-violet-500 w-[560px]">
+      <h1 className="text-5xl mb-10">評審機制</h1>
+      <p className="text-[22px]">初選：將由六角學院前端、UI 評審進行第一波篩選，並於 12/5（五）公布初選佳作名單</p>
+      <p className="text-[22px]">決選：由三大企業針對該企業主題進行入圍獎最後篩選，並於 12/23（五）公布企業得獎名單</p>
+    </div>
+  )
+}
