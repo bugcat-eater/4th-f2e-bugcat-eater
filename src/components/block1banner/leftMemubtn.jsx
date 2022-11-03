21b888be15614a3345ef34ce00952e025cdd6d83
Ted <wodf410@gmail.com>
2022-11-04 00:09:05 +0800

    done banner

---
+++ src/components/block1banner/leftMemubtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LeftMemubtn({text}) {
+  return (
+    <a className=' inline-block mx-5 max-lg:hidden text-xs  align-middle   '>
+        {text}
+    </a>
+  )
+}
